9069c6dfe802417e5ea538cca937092659eb1288
Jovia Nierenberg <jovia.nierenberg@ucsf.edu>
2020-07-27 15:30:54 -0700

    finish preparing caviar input files, bash code to run caviar for all blocks

--- caviar_file_preparation.R
+++ caviar_file_preparation.R
@@ -12,31 +12,40 @@ ld_map3k1 = fread("/zivlab/data3/jnierenberg/Data/rare_variant_functional_map3k1
 # function to create caviar input files for one block at a given rsq threshold
 create_caviar_files_for_block = function(rsq_threshold, block_num){
   block_num_col = paste0("block_num_", rsq_threshold)
-  # prepare z-score file
-  z_file = results_map3k1 %>% filter(get(block_num_col)==block_num) %>% 
-    mutate(z_score = bcac_onco_icogs_gwas_beta/bcac_onco_icogs_gwas_se) %>%
-    select(snp_id, z_score)
-  # create r (not rsq) matrix - sign is important
-  sign_betas_ordered = z_file %>% select(snp_id) %>% left_join(results_map3k1) %>% select(sign_beta) %>% pull()
-  sign_matrix = sign_betas_ordered %*% t(sign_betas_ordered)
-  r_matrix_unsigned = z_file %>% select(RS_number = snp_id) %>%
-    left_join(ld_map3k1) %>% 
-    select(z_file$snp_id) %>%
-    mutate_all(sqrt) 
-  r_matrix = r_matrix_unsigned*sign_matrix # multiply these together so that the correlations are signed
-  # write files
-  fwrite(z_file, file = paste0("/zivlab/data3/jnierenberg/Data/rare_variant_functional_map3k1/caviar/z_file_rsq_", rsq_threshold, "_block_", block_num, ".txt"), sep="\t",  col.names = FALSE)
-  fwrite(r_matrix, file = paste0("/zivlab/data3/jnierenberg/Data/rare_variant_functional_map3k1/caviar/r_matrix_rsq_", rsq_threshold, "_block_", block_num, ".txt"), sep="\t",  col.names = FALSE)
+  # count variants in block
+  current_block = results_map3k1 %>% filter(get(block_num_col)==block_num) 
+  print(paste0("block: ", block_num, ", number of variants: ", nrow(current_block)))
+  # for blocks with >1 variant, create caviar files
+  if (nrow(current_block)>1){
+    # prepare z-score file
+    z_file = current_block %>% 
+      mutate(z_score = bcac_onco_icogs_gwas_beta/bcac_onco_icogs_gwas_se) %>%
+      select(snp_id, z_score)
+    # create r (not rsq) matrix - sign is important
+    sign_betas_ordered = z_file %>% select(snp_id) %>% left_join(results_map3k1) %>% select(sign_beta) %>% pull()
+    sign_matrix = sign_betas_ordered %*% t(sign_betas_ordered)
+    r_matrix_unsigned = z_file %>% select(RS_number = snp_id) %>%
+      left_join(ld_map3k1) %>% 
+      select(z_file$snp_id) %>%
+      mutate_all(sqrt) 
+    r_matrix = r_matrix_unsigned*sign_matrix # multiply these together so that the correlations are signed
+    # write files
+    fwrite(z_file, file = paste0("/zivlab/data3/jnierenberg/Data/rare_variant_functional_map3k1/caviar/z_file_rsq_", rsq_threshold, "_block_", block_num, ".txt"), sep="\t",  col.names = FALSE)
+    fwrite(r_matrix, file = paste0("/zivlab/data3/jnierenberg/Data/rare_variant_functional_map3k1/caviar/r_matrix_rsq_", rsq_threshold, "_block_", block_num, ".txt"), sep="\t",  col.names = FALSE)
+  }
 }
-create_caviar_files_for_block("04", 1)
 
 # function to create caviar input files for an rsq threshold
-create_all_caviar_files = function(rsq_theshold){
+create_all_caviar_files = function(rsq_threshold){
   # identify number of blocks
-  # loop through blocks
+  block_num_col = paste0("block_num_", rsq_threshold)
+  highest_block_num = results_map3k1 %>% select({{block_num_col}}) %>% pull() %>% max(., na.rm = TRUE) 
+  # loop through blocks and create caviar files for those with >1 variant
+  for (block_number in 1:highest_block_num) {
+    create_caviar_files_for_block(rsq_threshold, block_number)
+  }
 }
 
-# will need to run the above for each haplotype with >1 variant
-
-# CAVIAR command (without last few parameters), from project caviar directory
-# CAVIAR -o caviar_out_rsq_04_block_1 -l r_matrix_rsq_04_block_1.txt -z z_file_rsq_04_block_1.txt
+# create files
+create_all_caviar_files("04")
+create_all_caviar_files("02")
